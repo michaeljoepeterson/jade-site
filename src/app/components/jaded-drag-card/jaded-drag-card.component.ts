7317a4f86f6022d6fb17a81605512095a18f70da
michael peterson <michaeljoepeterson@gmail.com>
2021-03-28 15:43:29 -0600

    first drag component

--- src/app/components/jaded-drag-card/jaded-drag-card.component.ts
+++ src/app/components/jaded-drag-card/jaded-drag-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-jaded-drag-card',
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jaded-drag-card.component.css']
 })
 export class JadedDragCardComponent implements OnInit {
+  @Input() cardHeaderText:string = "";
+  @Input() size:string = "";
 
-  constructor() { }
+  @ViewChild('cardHeader') cardHeaderRef: ElementRef;
+
+  @HostListener("window:resize",["event"])
+  onResize(event:any){
+    try{
+      if(this.footerStyles.height !== this.cardHeaderRef.nativeElement.offsetHeight){
+        this.setFooterHeight(this.cardHeaderRef.nativeElement.offsetHeight);
+      }
+    }
+    catch(e){
+      console.warn(e);
+    }
+  }
+
+  footerStyles:any = {};
+
+  constructor(
+    private ref:ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(){
+    if(!this.footerStyles.height){
+      this.setFooterHeight(this.cardHeaderRef.nativeElement.offsetHeight);
+    }
+  }
+
+  setFooterHeight(height:number){
+    this.footerStyles.height = height + 'px';
+    this.ref.detectChanges();
   }
 
 }
